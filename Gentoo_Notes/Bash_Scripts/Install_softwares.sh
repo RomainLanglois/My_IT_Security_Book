5a67166b8c199c46f9461b2dc7a8c5fbf7314069
RomainLanglois <rls_it@protonmail.com>
2022-06-24 12:02:52 +0200

    Removing unecessary content and adding a new package

--- Gentoo_Notes/Bash_Scripts/Install_softwares.sh
+++ Gentoo_Notes/Bash_Scripts/Install_softwares.sh
@@ -1,26 +1,5 @@
-# This list is based on my current /var/lib/portage/world file
 # Bash one liner to retrieve and format all packages inside the world file
 # while read -r line; do echo "\"$line\""; done < /var/lib/portage/world
-
-## Already existing packages
-# "app-admin/sudo"
-#"net-misc/dhcpcd"
-#"net-misc/netifrc"
-#"sys-boot/grub:2"
-#"sys-fs/cryptsetup"
-#"sys-fs/lvm2"
-#"sys-kernel/gentoo-sources"
-#"sys-kernel/linux-firmware"
-#"sys-kernel/dracut"
-
-## GUI Packages
-#"x11-base/xorg-drivers"
-#"x11-base/xorg-server"
-#"x11-misc/i3blocks"
-#"x11-misc/i3lock"
-#"x11-misc/i3status"
-#"x11-wm/i3-gaps"
-#"x11-terms/terminator"
 
 ## Add support for the TPM
 TPM_softwares_array=(
@@ -36,6 +15,7 @@ softwares_array=(
 "dev-vcs/git"
 "sys-apps/usbutils"
 "sys-process/htop"
+"app-arch/lz4"
 )
 
 emaint -a sync
